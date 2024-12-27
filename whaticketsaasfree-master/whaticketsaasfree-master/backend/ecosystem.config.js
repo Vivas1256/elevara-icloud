7619ee2dbe9fb014c8c76b66bb864ecda838a0cf
vivas1256 <trikool1010@hotmail.com>
2024-12-27 04:08:03 -0500

    trsduccion de backend español

--- whaticketsaasfree-master/whaticketsaasfree-master/backend/ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = [{
-  script: 'dist/server.js',
-  name: 'beta-back',
-  exec_mode: 'fork',
-  cron_restart: '05 00 * * *',
-  max_memory_restart: '769M', // Configuración para reiniciar al alcanzar 769 MB de memoria
-  node_args: '--max-old-space-size=769', // Limite de memória do Node.js para 769 MB
-  watch: false
-}]
